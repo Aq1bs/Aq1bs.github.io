6f3472d29e9a58f6f062b36a58538823cb8b0bdd
Aq1bs <akybshahid@gmail.com>
2025-03-30 02:34:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ updateCurrentTime(); // Call it once immediately to display the time right away
 
 // Define all the prayer times for the day
 const prayerTimes = [
-  { name: "Fajr", time: "05:41" },
+  { name: "Fajr", time: "04:41" },
   { name: "Sunrise", time: "06:40" },
   { name: "Dhuhr", time: "13:11" },
   { name: "Asr", time: "17:34" },
